561399a95b81bc8f45938fdf878b01aec85a9c91
test <test@gmail.com>
2021-05-11 00:26:30 +0800

    modified supplyModal

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,63 @@
 module.exports = {
-    "env": {
-      "browser": true,
-      "es6": true,
-      "jasmine": true,
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
     },
-    "parser": "babel-eslint",
-    "extends": ["airbnb", "plugin:prettier/recommended"],
-    "plugins": ["jsx-a11y",  "prettier"],
-    "rules": {
-      "import/extensions": 0,
-      "import/prefer-default-export": 0,
-      "import/no-extraneous-dependencies": 0,
-      "jsx-a11y/no-static-element-interactions": 0,
-      "jsx-a11y/click-events-have-key-events": 0,
-      "react/jsx-filename-extension": [1, {"extensions": [".js"]}],
-      "react/jsx-one-expression-per-line": 0,
-      "react/no-array-index-key": 0,
-      "react/forbid-prop-types": 0,
-      "react/destructuring-assignment": 0,
-      "react/jsx-wrap-multilines": 0,
-      "jsx-a11y/no-noninteractive-element-interactions": 0,
-      "camelcase": 0,
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
     },
-    "settings": {
-      "import/resolver": {
-        "node": {
-          "paths": ["src"]
-        }
-      },
-    },
-  };
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    // Make this the last element so prettier config overrides other formatting rules
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 'off',
+    'max-len': [2, { code: 110, tabWidth: 2, ignoreUrls: true }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-wrap-multilines': ['error', { declaration: false, assignment: false }],
+    'global-require': 'off',
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'no-underscore-dangle': 'off',
+    'prefer-promise-reject-errors': 'off',
+    'no-nested-ternary': 'off',
+    'react/no-multi-comp': 'off',
+    'react/no-unescaped-entities': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-fragments': 'off',
+    'linebreak-style': 'off',
+    'react/state-in-constructor': 'off',
+    'object-curly-newline': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'function-paren-newline': 'off',
+    'consistent-return': 'off',
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'template-curly-spacing': 'off',
+    indent: 'off',
+    'operator-linebreak': 'off',
+    'arrow-body-style': 'off',
+    'no-unused-vars': 'off',
+    'react/prop-types': 0,
+  },
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    singleQuote: true
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    jsxBracketSameLine: false,
 };
