e8b5cc1ca409b8e28e37180a8abfa61bc7b7960d
Nick Wilson <nkx.wilson@gmail.com>
2021-06-24 17:55:00 +0500

    show auction bar

--- src/pages/Auction/item.js
+++ src/pages/Auction/item.js
@@ -2,111 +2,64 @@ import React, { useEffect } from 'react';
 import MiniLogo from '../../components/UI/MiniLogo';
 import { BlockChart } from '../../components/common/BlockChart';
 
-const data = [
-  {
-    value: 3,
-  },{
-    value: 3,
-  },{
-    value: 3,
-  },{
-    value: 3,
-  },{
-    value: 3,
+
+let blockChartOptions = {
+  // style of canvas and boundary to be plotted on to
+  canvas: {
+    background: {
+      style: 'transparent',
+    },
   },
-  {
-    value: 1,
+  padding: {
+    top: 0, // space between canvas and top of draw area
+    bottom: 0, // space between canvas and bottom of draw area
+    left: 0, // space between canvas and left of draw area
+    right: 0, // space between canvas and right of draw area
   },
-  {
-    value: 8,
+  boundary: {
+    background: {
+      style: 'transparent',
+    },
   },
-  {
-    value: 3,
+  yAxis: {
+    width: 0.5, // thickness of y axis
+    color: '#C4C4C4', // color of y axis
   },
-  {
-    value: 4,
+  xAxis: {
+    width: 0.5, // thickness of x axis
+    color: '#C4C4C4', // color of x axis
   },
-  {
-    value: 9,
+  // style and properties of blocks to be plotted
+  blocks: {
+    forceSquare: true,
+    space: {
+      horizontal: 0, // space between each column
+      vertical: 0, // space between each blocks in vertical
+    },
+    groupBy: 'value', // key used to group data into same column
+    background: {
+      style: 'custom', // custom background color. other supported color is 'scale', 'preset-scale'
+      color: '#aaaaaa', // default block background color
+      scaleTo: '#eacd31', // when scale style is used, color palette will be generate from color -> scaleTo color
+    },
+    shadow: {
+      style: 'custom', // custom shadow. other supported shadow is 'none'
+      color: '#aaaaaa',
+      blur: 1,
+      offsetX: 1,
+      offsetY: 1,
+    },
+    border: {
+      style: 'line',
+      width: 2,
+      color: '#000000',
+    },
   },
-  {
-    value: 9,
-  },
-  {
-    value: 9,
-  },
-  {
-    value: 9,
-  },
-  {
-    value: 9,
-  },
-  {
-    value: 10,
-  },
-  {
-    value: 2,
-  },
-  {
-    value: 3,
-  },
-];
+};
 function AuctionItem(props) {
   useEffect(() => {
-    const blockchart = new BlockChart(`myCanvas${props.id}`, {
-      // style of canvas and boundary to be plotted on to
-      canvas: {
-        background: {
-          style: 'transparent',
-        },
-      },
-      padding: {
-        top: 0, // space between canvas and top of draw area
-        bottom: 0, // space between canvas and bottom of draw area
-        left: 0, // space between canvas and left of draw area
-        right: 0, // space between canvas and right of draw area
-      },
-      boundary: {
-        background: {
-          style: 'transparent',
-        },
-      },
-      yAxis: {
-        width: 0.5, // thickness of y axis
-        color: '#C4C4C4', // color of y axis
-      },
-      xAxis: {
-        width: 0.5, // thickness of x axis
-        color: '#C4C4C4', // color of x axis
-      },
-      // style and properties of blocks to be plotted
-      blocks: {
-        forceSquare: true,
-        space: {
-          horizontal: 0, // space between each column
-          vertical: 0, // space between each blocks in vertical
-        },
-        groupBy: 'value', // key used to group data into same column
-        background: {
-          style: 'custom', // custom background color. other supported color is 'scale', 'preset-scale'
-          color: '#aaaaaa', // default block background color
-          scaleTo: '#eacd31', // when scale style is used, color palette will be generate from color -> scaleTo color
-        },
-        shadow: {
-          style: 'custom', // custom shadow. other supported shadow is 'none'
-          color: '#aaaaaa',
-          blur: 1,
-          offsetX: 1,
-          offsetY: 1,
-        },
-        border: {
-          style: 'line',
-          width: 2,
-          color: '#000000',
-        },
-      },
-    });
-    blockchart.loadData(data);
+    const blockchart = new BlockChart(`myCanvas${props.id}`, blockChartOptions);
+    blockchart.loadData(props.data);
   }, []);
   return (
     <div className="col-span-12 lg:col-span-4 md:col-span-6 bg-black rounded-2xl p-6 flex flex-col">
