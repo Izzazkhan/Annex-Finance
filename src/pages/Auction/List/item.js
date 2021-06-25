a444644a4143b28a1fe6a14127f57b398107552b
Nick Wilson <nkx.wilson@gmail.com>
2021-06-25 16:19:25 +0500

    auction detail page

--- src/pages/Auction/List/item.js
+++ src/pages/Auction/List/item.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import MiniLogo from '../../../components/UI/MiniLogo';
+import { Link } from 'react-router-dom';
 import { BlockChart } from '../../../components/common/BlockChart';
 import { useHistory } from 'react-router-dom';
 
@@ -67,53 +68,55 @@ function AuctionItem(props) {
   };
   return (
     <div className="col-span-12 lg:col-span-4 md:col-span-6 bg-black rounded-2xl p-6 flex flex-col">
-      <div className="text-white flex flex-row items-stretch justify-between items-center mb-8">
-        <div className="flex flex-col items-start justify-start ">
-          <div className="text-white text-2xl ">Non-Fungible Bible</div>
-          <div className="text-base font-normal">Auction id#1DPRC</div>
+      <Link to="detail">
+        <div className="text-white flex flex-row items-stretch justify-between items-center mb-8">
+          <div className="flex flex-col items-start justify-start ">
+            <div className="text-white text-2xl ">Non-Fungible Bible</div>
+            <div className="text-base font-normal">Auction id#1DPRC</div>
+          </div>
+          <div className="flex flex-col items-center ">
+            <MiniLogo size="sm" />
+            <div className="text-sm mt-2">Live</div>
+          </div>
         </div>
-        <div className="flex flex-col items-center ">
-          <MiniLogo size="sm" />
-          <div className="text-sm mt-2">Live</div>
-        </div>
-      </div>
-      <div className="graph">
-        <div className="flex items-end">
-          <div className="graph-left-label flex flex-col items-center text-white text-sm mt-8 justify-center font-normal">
-            <span className="border first"></span>
-            <span className="label my-2 font-normal">
-              No. of share <b>50</b>
+        <div className="graph">
+          <div className="flex items-end">
+            <div className="graph-left-label flex flex-col items-center text-white text-sm mt-8 justify-center font-normal">
+              <span className="border first"></span>
+              <span className="label my-2 font-normal">
+                No. of share <b>50</b>
+              </span>
+              <span className=" border last"></span>
+            </div>
+            {/* <img src={require('../../assets/images/graph.png').default} alt="" /> */}
+            <canvas id={`myCanvas${props.id}`} width="290" height="211"></canvas>
+          </div>
+
+          <div className="w-full graph-bottom-label flex items-center text-white text-sm mt-8 justify-center font-normal">
+            <span className="border first "></span>
+            <span className="label mx-2 font-normal">
+              Bid per share, sorted from lowest to highest
             </span>
-            <span className=" border last"></span>
-          </div>
-          {/* <img src={require('../../assets/images/graph.png').default} alt="" /> */}
-          <canvas id={`myCanvas${props.id}`} width="290" height="211"></canvas>
-        </div>
-
-        <div className="w-full graph-bottom-label flex items-center text-white text-sm mt-8 justify-center font-normal">
-          <span className="border first "></span>
-          <span className="label mx-2 font-normal">
-            Bid per share, sorted from lowest to highest
-          </span>
-          <span className=" border last "></span>
-        </div>
-      </div>
-
-      <div className="text-white flex flex-row items-stretch justify-between items-center mt-8">
-        <div className="items-start " onClick={() => redirectToUrl('/auction/detail')}>
-          <div className="text-primary text-sm font-normal">Batch auction</div>
-        </div>
-        <div className="items-center " onClick={() => redirectToUrl('/auction/detail')}>
-          <div className="flex items-center text-primary text-sm font-bold">
-            Enter
-            <img
-              className="ml-2"
-              src={require('../../../assets/images/enter-icon.png').default}
-              alt=""
-            />
+            <span className=" border last "></span>
           </div>
         </div>
-      </div>
+
+        <div className="text-white flex flex-row items-stretch justify-between items-center mt-8">
+          <div className="items-start " onClick={() => redirectToUrl('/auction/detail')}>
+            <div className="text-primary text-sm font-normal">Batch auction</div>
+          </div>
+          <div className="items-center " onClick={() => redirectToUrl('/auction/detail')}>
+            <div className="flex items-center text-primary text-sm font-bold">
+              Enter
+              <img
+                className="ml-2"
+                src={require('../../../assets/images/enter-icon.png').default}
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
