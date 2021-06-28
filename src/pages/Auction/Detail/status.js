87a9c01d33fc05312e520cbe297538100ce730db
Nick Wilson <nkx.wilson@gmail.com>
2021-06-28 16:52:32 +0500

    auction status chart

--- src/pages/Auction/Detail/status.js
+++ src/pages/Auction/Detail/status.js
@@ -1,5 +1,114 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Countdown from 'react-countdown';
+// import { BlockChart } from '../../../components/common/BlockChart';
+import LineChart from '../../../components/common/LineChart';
+
+let blockChartOptions = {
+  // style of canvas and boundary to be plotted on to
+  canvas: {
+    background: {
+      style: 'transparent',
+    },
+  },
+  padding: {
+    top: 0, // space between canvas and top of draw area
+    bottom: 0, // space between canvas and bottom of draw area
+    left: 0, // space between canvas and left of draw area
+    right: 0, // space between canvas and right of draw area
+  },
+  boundary: {
+    background: {
+      style: 'transparent',
+    },
+  },
+  yAxis: {
+    width: 0.5, // thickness of y axis
+    color: '#C4C4C4', // color of y axis
+  },
+  xAxis: {
+    width: 0.5, // thickness of x axis
+    color: '#C4C4C4', // color of x axis
+  },
+  // style and properties of blocks to be plotted
+  blocks: {
+    forceSquare: true,
+    space: {
+      horizontal: 0, // space between each column
+      vertical: 0, // space between each blocks in vertical
+    },
+    groupBy: 'value', // key used to group data into same column
+    background: {
+      style: 'custom', // custom background color. other supported color is 'scale', 'preset-scale'
+      color: '#aaaaaa', // default block background color
+      scaleTo: '#eacd31', // when scale style is used, color palette will be generate from color -> scaleTo color
+    },
+    shadow: {
+      style: 'custom', // custom shadow. other supported shadow is 'none'
+      color: '#aaaaaa',
+      blur: 1,
+      offsetX: 1,
+      offsetY: 1,
+    },
+    border: {
+      style: 'line',
+      width: 2,
+      color: '#000000',
+    },
+  },
+};
+const graphData = [
+  {
+    value: 3,
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 1,
+  },
+  {
+    value: 8,
+  },
+  {
+    value: 3,
+  },
+  {
+    value: 4,
+  },
+  {
+    value: 9,
+  },
+  {
+    value: 9,
+  },
+  {
+    value: 9,
+  },
+  {
+    value: 9,
+  },
+  {
+    value: 9,
+  },
+  {
+    value: 10,
+  },
+  {
+    value: 2,
+  },
+  {
+    value: 3,
+  },
+];
 
 const AuctionStatus = ({ auctionEndDate, label }) => {
   return (
@@ -57,18 +166,26 @@ const AuctionCompleted = () => {
         <img className="ml-3" src={require('../../../assets/images/check.svg').default} alt="" />
         <div className="text-white text-4xl mt-10 mb-3">Auction Finished Successfully</div>
         <div className="text-white text-base mb-10">you are able to claim 1 Non-Fungible Bible</div>
-        <button className="focus:outline-none py-2 px-12 text-black text-xl 2xl:text-24
-         h-14 bg-white rounded-lg bgPrimaryGradient rounded-lg">Claim</button>
+        <button
+          className="focus:outline-none py-2 px-12 text-black text-xl 2xl:text-24
+         h-14 bg-white rounded-lg bgPrimaryGradient rounded-lg"
+        >
+          Claim
+        </button>
       </div>
     </div>
   );
 };
-
 const AuctionProgress = () => {
+//   useEffect(() => {
+//     const blockchart = new BlockChart(`myCanvas`, blockChartOptions);
+//     blockchart.loadData(graphData);
+//   }, []);
   return (
     <>
       <div className="text-white flex flex-col items-stretch justify-between items-center p-6 border-b border-lightGray">
-        graph
+        {/* <canvas id={`myCanvas`}></canvas> */}
+        <LineChart width="100%" height="211px" />
       </div>
       <div className="text-white flex flex-col items-stretch justify-between items-center p-6 ">
         <div className="">
@@ -86,23 +203,35 @@ const AuctionProgress = () => {
             <input className="w-full" type="range" min="0" max="951.7" />
           </div>
           <div className="mb-7">
-            <input className="border border-solid border-gray bg-transparent
-                           rounded-xl w-full focus:outline-none font-bold px-4 h-14 text-white" type="text" />
+            <input
+              className="border border-solid border-gray bg-transparent
+                           rounded-xl w-full focus:outline-none font-bold px-4 h-14 text-white"
+              type="text"
+            />
           </div>
           <div className="mb-7">
             <div className="label flex flex-row justify-between items-center mb-4">
               <div className="flex flex-col">
                 <div className="text-md text-primary font-bold mb-2 ">Commitment</div>
-                <div className="text-md ">Sed a condimentum nisl. Nulla mi libero, pretium sit amet posuere in,
-                  iaculis eu lectus. Aenean a urna vitae risus ullamcorpe.</div>
+                <div className="text-md ">
+                  Sed a condimentum nisl. Nulla mi libero, pretium sit amet posuere in, iaculis eu
+                  lectus. Aenean a urna vitae risus ullamcorpe.
+                </div>
               </div>
             </div>
           </div>
           <div className="input-with-button flex">
-            <input className="w-full border border-solid border-primary bg-transparent
-                           rounded-xl w-full focus:outline-none font-bold px-4 pr-10 h-14 text-white" type="text" />
-            <button className="focus:outline-none py-2 px-12 text-black text-xl 2xl:text-24
-         h-14 bg-white rounded-lg bg-primary rounded-lg">Commit</button>
+            <input
+              className="w-full border border-solid border-primary bg-transparent
+                           rounded-xl w-full focus:outline-none font-bold px-4 pr-10 h-14 text-white"
+              type="text"
+            />
+            <button
+              className="focus:outline-none py-2 px-12 text-black text-xl 2xl:text-24
+         h-14 bg-white rounded-lg bg-primary rounded-lg"
+            >
+              Commit
+            </button>
           </div>
         </div>
       </div>
