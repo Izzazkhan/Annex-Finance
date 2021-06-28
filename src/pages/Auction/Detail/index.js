a291c09eda7e646bf1d0568fea17a8a402e108a0
Nick Wilson <nkx.wilson@gmail.com>
2021-06-28 10:04:36 +0500

    auction detail status

--- src/pages/Auction/Detail/index.js
+++ src/pages/Auction/Detail/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Table from './Table';
 import Progress from '../../../components/UI/Progress';
-
+import AuctionStatus from './status';
 function Detail(props) {
   return (
     <div>
@@ -160,40 +160,13 @@ function Detail(props) {
           </div>
         </div>
         <div className="col-span-4 bg-fadeBlack rounded-2xl flex flex-col justify-between">
-          <div className="text-white flex flex-row items-stretch justify-between items-center  p-6 border-b border-lightGray">
-            <div className="flex flex-col items-start justify-start ">
-              <div className="text-white text-2xl ">Auction Progress</div>
-              <div className="text-base font-normal opacity-0 "> text</div>
-            </div>
-          </div>
-          <div className="flex-1 text-white flex flex-row items-stretch justify-between items-center  p-6">
-            <div className="w-full flex flex-col items-center justify-center ">
-              <div className="text-white text-4xl mb-10">Countdown</div>
-              <div className="counter bg-primary flex flex-row items-center py-10 rounded-2xl">
-                <div className="flex flex-col items-center px-6 border-r border-lightprimary">
-                  <div className="text-3xl font-bold">01</div>
-                  <div className="text-2xl font-normal">DAYs</div>
-                </div>
-                <div className="flex flex-col items-center px-6 border-r border-lightprimary">
-                  <div className="text-3xl font-bold">07</div>
-                  <div className="text-2xl font-normal">HOURS</div>
-                </div>
-                <div className="flex flex-col items-center px-6 border-r border-lightprimary">
-                  <div className="text-3xl font-bold">50</div>
-                  <div className="text-2xl font-normal">MIN</div>
-                </div>
-                <div className="flex flex-col items-center px-6">
-                  <div className="text-3xl font-bold">32</div>
-                  <div className="text-3xl font-normal">SEC</div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <AuctionStatus />
         </div>
       </div>
       <Table />
     </div>
   );
 }
+
 
 export default Detail;
---
+++ src/pages/Auction/Detail/status.js
@@ -0,0 +1,127 @@
+const AuctionStatus = () => {
+  return (
+    <Fragment>
+      <div className="text-white flex flex-row items-stretch justify-between items-center  p-6 border-b border-lightGray">
+        <div className="flex flex-col items-start justify-start ">
+          <div className="text-white text-2xl ">Auction Progress</div>
+          <div className="text-base font-normal opacity-0 "> text</div>
+        </div>
+      </div>
+      <AuctionCountDown />
+    </Fragment>
+  );
+};
+
+const AuctionCountDown = () => {
+  return (
+    <div className="flex-1 text-white flex flex-row items-stretch justify-between items-center  p-6">
+      <div className="w-full flex flex-col items-center justify-center ">
+        <div className="text-white text-4xl mb-10">Countdown</div>
+        <div className="counter bg-primary flex flex-row items-center py-10 rounded-2xl">
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">01</div>
+            <div className="text-2xl font-normal">DAYs</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">07</div>
+            <div className="text-2xl font-normal">HOURS</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">50</div>
+            <div className="text-2xl font-normal">MIN</div>
+          </div>
+          <div className="flex flex-col items-center px-6">
+            <div className="text-3xl font-bold">32</div>
+            <div className="text-3xl font-normal">SEC</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AuctionCompleted = () => {
+  return (
+    <div className="flex-1 text-white flex flex-row items-stretch justify-between items-center  p-6">
+      <div className="w-full flex flex-col items-center justify-center ">
+        <div className="text-white text-4xl mb-10">Countdown</div>
+        <div className="counter bg-primary flex flex-row items-center py-10 rounded-2xl">
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">01</div>
+            <div className="text-2xl font-normal">DAYs</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">07</div>
+            <div className="text-2xl font-normal">HOURS</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">50</div>
+            <div className="text-2xl font-normal">MIN</div>
+          </div>
+          <div className="flex flex-col items-center px-6">
+            <div className="text-3xl font-bold">32</div>
+            <div className="text-3xl font-normal">SEC</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AuctionProgress = () => {
+  return (
+    <div className="flex-1 text-white flex flex-row items-stretch justify-between items-center  p-6">
+      <div className="w-full flex flex-col items-center justify-center ">
+        <div className="text-white text-4xl mb-10">Countdown</div>
+        <div className="counter bg-primary flex flex-row items-center py-10 rounded-2xl">
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">01</div>
+            <div className="text-2xl font-normal">DAYs</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">07</div>
+            <div className="text-2xl font-normal">HOURS</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">50</div>
+            <div className="text-2xl font-normal">MIN</div>
+          </div>
+          <div className="flex flex-col items-center px-6">
+            <div className="text-3xl font-bold">32</div>
+            <div className="text-3xl font-normal">SEC</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const AuctionClaim = () => {
+  return (
+    <div className="flex-1 text-white flex flex-row items-stretch justify-between items-center  p-6">
+      <div className="w-full flex flex-col items-center justify-center ">
+        <div className="text-white text-4xl mb-10">Countdown</div>
+        <div className="counter bg-primary flex flex-row items-center py-10 rounded-2xl">
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">01</div>
+            <div className="text-2xl font-normal">DAYs</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">07</div>
+            <div className="text-2xl font-normal">HOURS</div>
+          </div>
+          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+            <div className="text-3xl font-bold">50</div>
+            <div className="text-2xl font-normal">MIN</div>
+          </div>
+          <div className="flex flex-col items-center px-6">
+            <div className="text-3xl font-bold">32</div>
+            <div className="text-3xl font-normal">SEC</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuctionStatus;
