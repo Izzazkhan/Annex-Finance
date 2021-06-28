f0bb6e08f2a55e5d738c3b95d76f53b81eed2473
Nick Wilson <nkx.wilson@gmail.com>
2021-06-28 11:37:02 +0500

    dynamic countdown

--- src/pages/Auction/Detail/index.js
+++ src/pages/Auction/Detail/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Countdown from 'react-countdown';
 import Table from './Table';
 import Progress from '../../../components/UI/Progress';
 import AuctionStatus from './status';
+import moment from 'moment';
+
+const auctionEndDate = moment().add(1, 'minutes').toDate().getTime();
+const auctionStartDate = moment().toDate().getTime();
+
 function Detail(props) {
   return (
     <div>
@@ -36,27 +42,18 @@ function Detail(props) {
           <h2 className="text-white mb-1 text-3xl font-bold text-primary">05:01:25</h2>
           <div className="text-white text-3xl font-bold">ENDS IN</div>
         </div> */}
-        <div className="flex flex-col justify-between items-center order-last md:order-2">
-          <div className="relative mb-2">
-            <Progress
-              wrapperClassName="hidden md:block"
-              type="circle"
-              width={200}
-              percent={0}
-              strokeWidth={4}
+        <Countdown
+          date={auctionEndDate}
+          renderer={(props) => (
+            <ProgressBar
+              {...props}
+              auctionEndDate={auctionEndDate}
+              auctionStartDate={auctionStartDate}
+              
             />
-            <div
-              className={`flex flex-col items-center absolute top-1/2 left-1/2 
-                            w-full h-full pt-18 md:pt-14 pb-14 md:pb-10 px-4
-                            transform -translate-x-1/2 -translate-y-1/2 justify-center`}
-            >
-              <div className="flex flex-col items-center space-y-1 md:space-y-2 mb-3 md:mb-3 flex-grow text-center">
-                <div className="text-primary font-bold text-lg md:text-xl">05:01:25</div>
-                <div className="text-white font-bold text-xl md:text-2xl">ENDS IN</div>
-              </div>
-            </div>
-          </div>
-        </div>
+          )}
+        />
+
         <div className="col-span-6 lg:col-span-3 md:col-span-6  px-8 flex flex-col border-r border-lightGray flex-1">
           <h2 className="flex items-center text-white mb-1 text-3xl font-bold text-primary">
             250 WETH{' '}
@@ -160,7 +157,7 @@ function Detail(props) {
           </div>
         </div>
         <div className="col-span-4 bg-fadeBlack rounded-2xl flex flex-col justify-between">
-          <AuctionStatus />
+          <AuctionStatus auctionEndDate={auctionEndDate}/>
         </div>
       </div>
       <Table />
@@ -168,4 +165,47 @@ function Detail(props) {
   );
 }
 
+const ProgressBar = ({
+  days,
+  hours,
+  minutes,
+  seconds,
+  completed,
+  auctionEndDate,
+  auctionStartDate,
+}) => {
+  let currentTimeStamp = Date.now();
+  let percentage =
+    ((currentTimeStamp - auctionStartDate) / (auctionEndDate - auctionStartDate)) * 100;
+  return (
+    <div className="flex flex-col justify-between items-center order-last md:order-2">
+      <div className="relative mb-2">
+        <Progress
+          wrapperClassName="hidden md:block"
+          type="circle"
+          width={200}
+          percent={percentage || 0}
+          strokeWidth={4}
+          color="#FFAB2D"
+          trailColor="#101016"
+        />
+        <div
+          className={`flex flex-col items-center absolute top-1/2 left-1/2 
+                    w-full h-full pt-18 md:pt-14 pb-14 md:pb-10 px-4
+                    transform -translate-x-1/2 -translate-y-1/2 justify-center`}
+        >
+          <div className="flex flex-col items-center space-y-1 md:space-y-2 mb-3 md:mb-3 flex-grow text-center">
+            <div className="text-primary font-bold text-lg md:text-xl">
+              {' '}
+              {days}:{hours}:{minutes}:{seconds}
+            </div>
+            <div className="text-white font-bold text-xl md:text-2xl">
+              {completed ? 'Completed' : 'ENDS IN'}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Detail;
--- src/pages/Auction/Detail/status.js
+++ src/pages/Auction/Detail/status.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
+import Countdown from 'react-countdown';
 
-const AuctionStatus = () => {
+const AuctionStatus = ({ auctionEndDate }) => {
   return (
     <Fragment>
       <div className="text-white flex flex-row items-stretch justify-between items-center  p-6 border-b border-lightGray">
@@ -9,33 +10,40 @@ const AuctionStatus = () => {
           <div className="text-base font-normal opacity-0 "> text</div>
         </div>
       </div>
-      <AuctionCountDown />
+      <AuctionCountDown auctionEndDate={auctionEndDate} />
     </Fragment>
   );
 };
 
-const AuctionCountDown = () => {
+const AuctionCountDown = ({ auctionEndDate }) => {
   return (
     <div className="flex-1 text-white flex flex-row items-stretch justify-between items-center  p-6">
       <div className="w-full flex flex-col items-center justify-center ">
         <div className="text-white text-4xl mb-10">Countdown</div>
         <div className="counter bg-primary flex flex-row items-center py-10 rounded-2xl">
-          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
-            <div className="text-3xl font-bold">01</div>
-            <div className="text-2xl font-normal">DAYs</div>
-          </div>
-          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
-            <div className="text-3xl font-bold">07</div>
-            <div className="text-2xl font-normal">HOURS</div>
-          </div>
-          <div className="flex flex-col items-center px-6 border-r border-lightprimary">
-            <div className="text-3xl font-bold">50</div>
-            <div className="text-2xl font-normal">MIN</div>
-          </div>
-          <div className="flex flex-col items-center px-6">
-            <div className="text-3xl font-bold">32</div>
-            <div className="text-3xl font-normal">SEC</div>
-          </div>
+          <Countdown
+            date={auctionEndDate}
+            renderer={({ days, hours, minutes, seconds }) => (
+              <Fragment>
+                <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+                  <div className="text-3xl font-bold">{days}</div>
+                  <div className="text-2xl font-normal">DAYs</div>
+                </div>
+                <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+                  <div className="text-3xl font-bold">{hours}</div>
+                  <div className="text-2xl font-normal">HOURS</div>
+                </div>
+                <div className="flex flex-col items-center px-6 border-r border-lightprimary">
+                  <div className="text-3xl font-bold">{minutes}</div>
+                  <div className="text-2xl font-normal">MIN</div>
+                </div>
+                <div className="flex flex-col items-center px-6">
+                  <div className="text-3xl font-bold">{seconds}</div>
+                  <div className="text-3xl font-normal">SEC</div>
+                </div>
+              </Fragment>
+            )}
+          />
         </div>
       </div>
     </div>
