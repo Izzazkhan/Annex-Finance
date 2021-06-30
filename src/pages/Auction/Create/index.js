532b0563fea43228b5e672232d9b795ca34cd455
Nick Wilson <nkx.wilson@gmail.com>
2021-06-30 15:03:35 +0500

    auction dailog

--- src/pages/Auction/Create/index.js
+++ src/pages/Auction/Create/index.js
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import Modal from './modal';
+
 export default function CreateAuction(props) {
+  const [showModal, updateShowModal] = useState(false);
   return (
     <div className="col-span-12 p-6 flex flex-col">
       <h2 className="text-white mb-2 text-4xl font-normal">Create Auction</h2>
+      <button
+        className="focus:outline-none bgPrimaryGradient py-2 px-4 rounded-3xl text-white"
+        onClick={() => {
+          updateShowModal(true);
+        }}
+      >
+        Show Modal
+      </button>
+      <Modal
+        open={showModal}
+        type="inprogress"
+        onSetOpen={() => updateShowModal(true)}
+        onCloseModal={() => updateShowModal(false)}
+      />
     </div>
   );
 }
---
+++ src/pages/Auction/Create/modal.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Modal from '../../../components/UI/Modal';
+import circleTick from '../../../assets/icons/circleTick.svg';
+
+const types = {
+  SUCCESS: 'success',
+  INPROGRESS: 'inprogress',
+};
+
+function AuctionModal({ open, onSetOpen, onCloseModal }) {
+  const title = (
+    <div className="text-center text-xl font-bold mt-4 mx-12 py-6 border-b border-solid border-gray-600">
+      Auction Submitting Confirmation
+    </div>
+  );
+
+  const content =
+    props.type === types['SUCCESS'] ? (
+      <div className="p-14">
+        <div className="flex flex-col items-center">
+          <img className="w-150px " src={circleTick} alt="transaction broadcast" />
+          <div className="text-xl font-bold mt-8">{`Auction Created Successfully`}</div>
+        </div>
+        <div className="flex justify-center mt-16">
+          <button
+            className="focus:outline-none bg-primary py-4 rounded text-2xl
+                 w-full max-w-350px text-black"
+            onClick={onCloseModal}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    ) : props.type === types['INPROGRESS'] ? (
+      <div className="p-14">
+        <div className="flex flex-col items-center">
+          <img className="w-150px " src={circleTick} alt="transaction broadcast" />
+          <div className="text-xl font-bold mt-8">{`Auction Created Successfully`}</div>
+        </div>
+        <div className="flex justify-center mt-16">
+          <button
+            className="focus:outline-none bg-primary py-4 rounded text-2xl
+                 w-full max-w-350px text-black"
+            onClick={onCloseModal}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    ) : (
+      ''
+    );
+
+  return (
+    <div>
+      <Modal
+        title={title}
+        content={content}
+        open={open}
+        onSetOpen={onSetOpen}
+        onCloseModal={onCloseModal}
+        afterCloseModal={() => {}}
+        width="max-w-xl"
+      />
+    </div>
+  );
+}
+
+export default AuctionModal;
