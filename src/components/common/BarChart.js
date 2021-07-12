0b89bf3b89108732c34bc36c2202b870dab38dcd
Nick Wilson <nkx.wilson@gmail.com>
2021-07-12 17:33:53 +0500

    update auction color in graph

--- src/components/common/BarChart.js
+++ src/components/common/BarChart.js
@@ -7,21 +7,20 @@ export default function Chart(props) {
       style={{
         width: '100%',
         height: props.height,
-        marginBottom:'-29px'
+        marginBottom: '-29px',
       }}
     >
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={props.data} margin={{ top: 0, left: 0, right: 0, bottom: 0 }}>
           <Bar dataKey="pv" fill="#C4C4C4">
             {props.data.map((entry, index) => {
-              const color = entry.isSuccessfull ? '#565656' : '#C4C4C4';
+              const color = entry.isSuccessfull ? '#C4C4C4' : '#565656';
               return <Cell fill={color} key={index} />;
             })}
           </Bar>
           <XAxis fontSize="12" key="name" />
         </BarChart>
       </ResponsiveContainer>
-    
     </div>
   );
 }
