ffe5b6558e72f288db70b76be158087e7f6eb1a1
Izaz Khan <izazkhan2000@gmail.com>
2021-10-18 18:36:18 +0500

    Line chart set

--- src/components/common/LineChart.js
+++ src/components/common/LineChart.js
@@ -7,7 +7,7 @@ export default function Chart({ width, height, data, biddingSymbol }) {
       className="relative pt-10"
       style={{ width: '100%', height, border: '1px solid #2E2E2E', borderTop: 'none' }}
     >
-      {/* <div className="absolute left-2 top-0 left-top flex flex-col">
+      <div className="absolute left-2 top-0 left-top flex flex-col">
         <div className="text-xs font-normal">Starting Price</div>
         <div className="text-base font-bold">
           {data[0].value} {biddingSymbol}
@@ -18,7 +18,7 @@ export default function Chart({ width, height, data, biddingSymbol }) {
         <div className="text-base font-bold">
           {data[1].value} {biddingSymbol}
         </div>
-      </div> */}
+      </div>
       <div className="absolute left-2 bottom-2 right-top flex flex-col text-left">
         <div className="text-xs font-normal text-icon">
           <span className="upcoming-icon"></span>Current Token Price
