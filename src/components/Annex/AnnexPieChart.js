8208a809e6de175bff72eb1193e4f2abe45b75fe
MayankGhrix <mayanky@ghrix.com>
2021-11-08 14:06:36 +0530

    infinite data in market details, piechart nan value handle in vote all

--- src/components/Annex/AnnexPieChart.js
+++ src/components/Annex/AnnexPieChart.js
@@ -1,8 +1,16 @@
+import { useMemo } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const AnnexPieChart = ({
     data,
 }) => {
+    const label = useMemo(() => {
+        let obj = data.reduce((a, b) => ({ value: a.value + b.value }))
+        if (data.length === 0 || !obj || isNaN(obj.value)) {
+            return "0"
+        }
+        return obj.value
+    }, [data])
     return (
         <div className="relative flex flex-col items-center justify-center">
             <PieChart width={110} height={110}>
@@ -23,7 +31,7 @@ const AnnexPieChart = ({
                 className="text-white absolute"
                 style={{ top: '50%', left: '50%', transform: "translate(-50%, -50%)" }}
             >
-                {data.length && data.reduce((a, b) => ({ value: a.value + b.value })).value}
+                {label}
             </span>
         </div>
     )
