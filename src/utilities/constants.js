6e48a5bd93d7755187f327d27e877695a806c0a3
sharozdev <sharozdev@gmail.com>
2021-11-10 14:46:47 +0500

    Addresses names changed

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -608,8 +608,8 @@ export const REACT_APP_ANN_Vault_ADDRESS = process.env.REACT_APP_ENV === 'dev' ?
   process.env.REACT_APP_ANN_TEST_Vault_ADDRESS
   : process.env.REACT_APP_ANN_MAIN_Vault_ADDRESS
 
-export const REACT_APP_ANNEX_FARM_ADDRESS = process.env.REACT_APP_ENV === 'dev' ? process.env.REACT_APP_TEST_Annex_Farm_ADDRESS
-  : process.env.REACT_APP_MAIN_Annex_Farm_ADDRESS
+export const REACT_APP_ANNEX_FARM_ADDRESS = process.env.REACT_APP_ENV === 'dev' ? process.env.REACT_APP_BSCTEST_Annex_Farm_ADDRESS
+  : process.env.REACT_APP_BSC_Annex_Farm_ADDRESS
 
 
 export const CONTRACT_DUTCH_AUCTION_ADDRESS =
