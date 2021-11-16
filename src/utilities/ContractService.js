d5f3e3d0433ecb667b684e1c9f15f6bdde0697ab
annultra <ozren@annex.finance>
2021-11-16 09:15:40 +0100

    added link and xrp

--- src/utilities/ContractService.js
+++ src/utilities/ContractService.js
@@ -19,6 +19,8 @@ const TOKEN_ABI = {
   tusd: constants.CONTRACT_TUSD_TOKEN_ABI,
   xvs: constants.CONTRACT_XVS_TOKEN_ABI,
   cake: constants.CONTRACT_CAKE_TOKEN_ABI,
+  link: constants.CONTRACT_LINK_TOKEN_ABI,
+  xrp: constants.CONTRACT_XRP_TOKEN_ABI,
 };
 const AUCTION_ABI = {
   batch: constants.CONTRACT_ANNEX_BATCH_AUCTION_ABI,
