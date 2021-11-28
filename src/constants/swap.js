b8ea3320b4405622e27e812297aa1290129a36d3
annultra <ozren@annex.finance>
2021-11-28 11:41:49 +0100

    added new USDC pair on bsc

--- src/constants/swap.js
+++ src/constants/swap.js
@@ -17,6 +17,13 @@ export const USDT = new Token(
 	"USDT",
 	"Tether USD"
 );
+export const USDC = new Token(
+	ChainId.MAINNET,
+	"0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d",
+	18,
+	"USDC",
+	"USD Coin"
+);
 
 const WETH_ONLY = {
 	[ChainId.MAINNET]: [WETH[ChainId.MAINNET]],
@@ -28,7 +35,7 @@ const WETH_ONLY = {
 // used to construct intermediary pairs for trading
 export const BASES_TO_CHECK_TRADES_AGAINST = {
 	...WETH_ONLY,
-	[ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], BUSD, USDT],
+	[ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], BUSD, USDT, USDC],
 };
 
 /**
@@ -44,7 +51,7 @@ export const CUSTOM_BASES = {
 // used for display in the default list when adding liquidity
 export const SUGGESTED_BASES = {
 	...WETH_ONLY,
-	[ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], BUSD, USDT],
+	[ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], BUSD, USDT, USDC],
 	[ChainId.CASSINI]: [
 		...WETH_ONLY[ChainId.CASSINI],
 		new Token(ChainId.CASSINI, "0x2C074fDeFc6613FA77d48332B5c57A013Ab85DCE", 18, "USDT", "Tether USD")
@@ -60,7 +67,7 @@ export const SUGGESTED_BASES = {
 // used to construct the list of all pairs we consider by default in the frontend
 export const BASES_TO_TRACK_LIQUIDITY_FOR = {
 	...WETH_ONLY,
-	[ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], BUSD, USDT],
+	[ChainId.MAINNET]: [...WETH_ONLY[ChainId.MAINNET], BUSD, USDT, USDC],
 	[ChainId.CASSINI]: [
 		...WETH_ONLY[ChainId.CASSINI],
 		new Token(ChainId.CASSINI, "0x2C074fDeFc6613FA77d48332B5c57A013Ab85DCE", 18, "USDT", "Tether USD"),
